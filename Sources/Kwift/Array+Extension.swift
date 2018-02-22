143bb0bda10e4239b0ba46b876680c1f8da821af
Kojirou <Kojirouhtc@gmail.com>
2018-02-23 00:35:04 +0800

    update

--- Sources/Kwift/Array+Extension.swift
+++ Sources/Kwift/Array+Extension.swift
@@ -42,3 +42,11 @@ extension Array where Element: Comparable {
         }
     }
 }
+
+#if !swift(>=3.3) || (swift(>=4) && !swift(>=4.1))
+extension Array {
+    func compactMap<T>(_ transform: (Element) throws -> T?) rethrows -> [T] {
+        return try flatMap { arg -> [T] in try transform(arg).map { [$0] } ?? [] }
+    }
+}
+#endif

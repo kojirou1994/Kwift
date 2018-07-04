0911a1ff56a6c4d6228c75720abc2b160d6ecea8
Kojirou <Kojirouhtc@gmail.com>
2018-07-05 00:13:39 +0800

    add 2 new Result types

--- Sources/Kwift/Result.swift
+++ Sources/Kwift/Result.swift
@@ -11,3 +11,19 @@ public enum Result<T> {
     case success(T)
     case failed(Error)
 }
+
+public enum ResultWithError<T, E: Error> {
+    case success(T)
+    case failed(E)
+}
+
+
+public struct ResultClosure<T> {
+    public var success: ((_ result: T) -> ())?
+    public var failure: ((_ error: Error) -> ())?
+    
+    public init(success: ((T) -> ())? = nil, failure: ((Error) -> ())? = nil) {
+        self.success = success
+        self.failure = failure
+    }
+}

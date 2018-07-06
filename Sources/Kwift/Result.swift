9a5e4142b8b9b3acd8b98e79da9b41d28d93aef2
Kojirou <Kojirouhtc@gmail.com>
2018-07-06 12:36:47 +0800

    update

--- Sources/Kwift/Result.swift
+++ Sources/Kwift/Result.swift
@@ -7,23 +7,53 @@
 
 import Foundation
 
-public enum Result<T> {
-    case success(T)
-    case failed(Error)
+public enum Result<Value> {
+    case success(Value)
+    case failure(Error)
 }
 
-public enum ResultWithError<T, E: Error> {
-    case success(T)
-    case failed(E)
-}
-
-
-public struct ResultClosure<T> {
-    public var success: ((_ result: T) -> ())?
-    public var failure: ((_ error: Error) -> ())?
+extension Result {
     
-    public init(success: ((T) -> ())? = nil, failure: ((Error) -> ())? = nil) {
+    public func resolve() throws -> Value {
+        switch self {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            throw error
+        }
+    }
+    
+}
+
+public enum ResultWithError<Value, E: Error> {
+    case success(Value)
+    case failure(E)
+}
+
+extension ResultWithError {
+    
+    public func resolve() throws -> Value {
+        switch self {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            throw error
+        }
+    }
+    
+}
+
+public struct ResultClosure<Value> {
+    
+    public var success: Handler<Value>?
+    public var failure: Handler<Error>?
+    public var completion: Handler<Result<Value>>?
+    
+    public typealias Handler<T> = (T) -> ()
+    
+    public init(success: Handler<Value>? = nil, failure: Handler<Error>? = nil, completion: Handler<Result<Value>>? = nil) {
         self.success = success
         self.failure = failure
+        self.completion = completion
     }
 }

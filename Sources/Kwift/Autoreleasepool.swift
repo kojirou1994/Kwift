8814eff7ebf9ab9d91d08556fcf1acb6a409da37
Kojirou <Kojirouhtc@gmail.com>
2018-08-28 00:01:12 +0800

    update

--- Sources/Kwift/Autoreleasepool.swift
+++ Sources/Kwift/Autoreleasepool.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 
+#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
 public func autoreleasepoolIfDarwin<Result>(invoking body: () throws -> Result) rethrows -> Result {
-    #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
     return try autoreleasepool(invoking: body)
-    #else
-    return try body()
-    #endif
 }
+#else
+public func autoreleasepoolIfDarwin<Result>(invoking body: () throws -> Result) rethrows -> Result {
+    return try body()
+}
+#endif

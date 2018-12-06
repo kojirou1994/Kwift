dcac9efc78c01caa3ee3562326a08d7b82e66505
Kojirou <Kojirouhtc@gmail.com>
2018-12-07 00:21:39 +0800

    update

--- Sources/Kwift/Executable.swift
+++ Sources/Kwift/Executable.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-public protocol Executable {
+#if os(macOS) || os(Linux)
+public protocol Executable: CustomStringConvertible {
     
     static var executableName: String {get}
     
@@ -39,4 +40,50 @@ extension Executable {
         return p
     }
     
+    public var description: String {
+        return "CommandLine: \(Self.executableName) \(arguments.joined(separator: " "))"
+    }
+    
+    public var commandLine: [String] {
+        return [Self.executableName] + arguments
+    }
+    
 }
+
+public class ParallelProcess {
+    
+    let q: OperationQueue
+    
+    public init() {
+        q = OperationQueue.init()
+    }
+    
+    public var maxConcurrentCount: Int {
+        set {
+            q.maxConcurrentOperationCount = newValue
+        }
+        
+        get {
+            return q.maxConcurrentOperationCount
+        }
+    }
+    
+    public func add(_ process: Process) {
+        q.addOperation {
+            process.launch()
+            process.waitUntilExit()
+        }
+    }
+    
+    public func add(_ executable: Executable) {
+        q.addOperation {
+            _ = try? executable.runAndWait()
+        }
+    }
+    
+    public func waitUntilAllOProcessesAreFinished() {
+        q.waitUntilAllOperationsAreFinished()
+    }
+    
+}
+#endif

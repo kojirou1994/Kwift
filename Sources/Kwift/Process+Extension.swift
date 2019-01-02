5d617981f780bb718c4430fe2fdc3f119dcf1a87
Kojirou <Kojirouhtc@gmail.com>
2019-01-02 21:20:01 +0800

    remove Result

--- Sources/Kwift/Process+Extension.swift
+++ Sources/Kwift/Process+Extension.swift
@@ -22,7 +22,7 @@ extension Process {
         let paths = PATH.split(separator: ":")
         for path in paths {
             let tmp = String(path).appendingPathComponent(executableName)
-            if FileManager.default.fileExists(atPath: tmp), FileManager.default.isExecutableFile(atPath: tmp) {
+            if FileManager.default.isExecutableFile(atPath: tmp) {
                 self.init()
                 if #available(OSX 10.13, *) {
                     self.executableURL = URL.init(fileURLWithPath: tmp)
@@ -38,9 +38,13 @@ extension Process {
     }
     
     @discardableResult
-    public static func run(_ args: [String], wait: Bool = false) throws -> Process {
+    public static func run(_ args: [String], wait: Bool = false,
+                           prepare: ((Process) -> Void)? = nil) throws -> Process {
         precondition(!args.isEmpty, "args must not be empty")
         let p = try Process.init(executableName: args[0], arguments: Array(args.dropFirst()))
+        if let prepare = prepare {
+            prepare(p)
+        }
         p.launch()
         if wait {
             p.waitUntilExit()
--- Sources/Kwift/Result.swift
+++ Sources/Kwift/Result.swift
@@ -7,53 +7,8 @@
 
 import Foundation
 
-public enum Result<Value> {
-    case success(Value)
-    case failure(Error)
-}
-
 extension Result {
     
-    public func resolve() throws -> Value {
-        switch self {
-        case .success(let value):
-            return value
-        case .failure(let error):
-            throw error
-        }
-    }
+    public typealias Completion = (Result<Success, Failure>) -> Void
     
-}
-
-public enum ResultWithError<Value, E: Error> {
-    case success(Value)
-    case failure(E)
-}
-
-extension ResultWithError {
-    
-    public func resolve() throws -> Value {
-        switch self {
-        case .success(let value):
-            return value
-        case .failure(let error):
-            throw error
-        }
-    }
-    
-}
-
-public struct ResultClosure<Value> {
-    
-    public var success: Handler<Value>?
-    public var failure: Handler<Error>?
-    public var completion: Handler<Result<Value>>?
-    
-    public typealias Handler<T> = (T) -> ()
-    
-    public init(success: Handler<Value>? = nil, failure: Handler<Error>? = nil, completion: Handler<Result<Value>>? = nil) {
-        self.success = success
-        self.failure = failure
-        self.completion = completion
-    }
 }

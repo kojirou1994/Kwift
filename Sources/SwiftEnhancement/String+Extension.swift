c1d6cfafc2451c52f31e651d06ecb901ba923232
kojirou <kojirouhtc@gmail.com>
2019-03-08 22:15:47 +0800

    add @inlinable to String subscript

--- Sources/SwiftEnhancement/String+Extension.swift
+++ Sources/SwiftEnhancement/String+Extension.swift
@@ -10,30 +10,46 @@ import Foundation
 
 extension String {
     
+    @usableFromInline
+    func calculateIndex(_ lowerBound: Int, _ upperBound: Int) -> (Index, Index) {
+        let lowerIndex = index(startIndex, offsetBy: lowerBound)
+        let upperIndex = index(lowerIndex, offsetBy: upperBound - lowerBound)
+        return (lowerIndex, upperIndex)
+    }
+    
+    @inlinable
     public subscript(bounds: Range<Int>) -> String.SubSequence {
-        return self[index(startIndex, offsetBy: bounds.lowerBound)..<index(startIndex, offsetBy: bounds.upperBound)]
+        let (lowerIndex, upperIndex) = calculateIndex(bounds.lowerBound, bounds.upperBound)
+        return self[lowerIndex..<upperIndex]
     }
     
+    @inlinable
     public subscript(bounds: ClosedRange<Int>) -> String.SubSequence {
-        return self[index(startIndex, offsetBy: bounds.lowerBound)...index(startIndex, offsetBy: bounds.upperBound)]
+        let (lowerIndex, upperIndex) = calculateIndex(bounds.lowerBound, bounds.upperBound)
+        return self[lowerIndex...upperIndex]
     }
     
+    @inlinable
     public subscript(bounds: PartialRangeFrom<Int>) -> String.SubSequence {
         return self[index(startIndex, offsetBy: bounds.lowerBound)...]
     }
     
+    @inlinable
     public subscript(bounds: PartialRangeUpTo<Int>) -> String.SubSequence {
         return self[..<index(startIndex, offsetBy: bounds.upperBound)]
     }
     
+    @inlinable
     public subscript(bounds: PartialRangeThrough<Int>) -> String.SubSequence {
         return self[...index(startIndex, offsetBy: bounds.upperBound)]
     }
     
+    @inlinable
     public subscript(i: Int) -> Character {
         return self[index(startIndex, offsetBy: i)]
     }
     
+    @inlinable
     public var lastPathComponent: String {
         return (self as NSString).lastPathComponent
     }
@@ -48,10 +64,12 @@ extension String {
         }
     }
     
+    @inlinable
     public var deletingLastPathComponent: String {
         return (self as NSString).deletingLastPathComponent
     }
     
+    @inlinable
     public func appendingPathComponent(_ str: String) -> String {
         if hasSuffix("/") {
             return appending(str)
@@ -60,18 +78,22 @@ extension String {
         }
     }
     
+    @inlinable
     public func appendingPathComponentURL(_ str: String) -> String {
         return (self as NSString).appendingPathComponent(str)
     }
     
+    @inlinable
     public var pathExtension: String {
         return (self as NSString).pathExtension
     }
     
+    @inlinable
     public var deletingPathExtension: String {
         return (self as NSString).deletingPathExtension
     }
     
+    @inlinable
     public func appendingPathExtension(_ str: String) -> String {
         return (self as NSString).appendingPathExtension(str)!
     }

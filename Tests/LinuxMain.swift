64cfe4042bfa16a889f3708451acb56b682558a0
kojirou <kojirouhtc@gmail.com>
2019-12-21 14:57:01 +0800

    regenerate linuxmain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,11 +1,9 @@
 import XCTest
 
-import ExecutableTests
 import KwiftExtensionTests
 import KwiftUtilityTests
 
 var tests = [XCTestCaseEntry]()
-tests += ExecutableTests.__allTests()
 tests += KwiftExtensionTests.__allTests()
 tests += KwiftUtilityTests.__allTests()
 

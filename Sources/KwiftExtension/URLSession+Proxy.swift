5c34de6a34dc260104df8de6bb24110899103918
kojirou <kojirouhtc@gmail.com>
2020-03-15 01:43:22 +0800

    rewrite Proxy methods

--- Sources/KwiftExtension/URLSession+Proxy.swift
+++ Sources/KwiftExtension/URLSession+Proxy.swift
@@ -2,15 +2,19 @@
 import Foundation
 
 extension URLSessionConfiguration {
-    
-    public enum ProxyType {
-        case disable
-        case http
-        case https
-        case socks5
+
+    public func disableProxy() {
+        if connectionProxyDictionary == nil {
+            connectionProxyDictionary = .init()
+        }
+        connectionProxyDictionary?.merge([
+            kCFNetworkProxiesHTTPEnable: false,
+            kCFNetworkProxiesHTTPSEnable: false,
+            kCFNetworkProxiesSOCKSEnable: false
+        ], uniquingKeysWith: {$1})
     }
 
-    public func setProxy(proxyType: ProxyType, host: String, port: Int) {
+    public func setProxy(proxyType: ProxyInfo.ProxyType, host: String, port: Int) {
         if connectionProxyDictionary == nil {
             connectionProxyDictionary = .init()
         }
@@ -33,46 +37,71 @@ extension URLSessionConfiguration {
                 kCFNetworkProxiesSOCKSProxy: host,
                 kCFNetworkProxiesSOCKSPort: port
             ], uniquingKeysWith: {$1})
-        case .disable:
-            connectionProxyDictionary?.merge([
-                kCFNetworkProxiesHTTPEnable: false,
-                kCFNetworkProxiesHTTPSEnable: false,
-                kCFNetworkProxiesSOCKSEnable: false
-            ], uniquingKeysWith: {$1})
         }
     }
     
     public func setProxy(environment: [String : String]) {
-        func readProxy(_ str: String?, type: ProxyType) {
-            guard let str = str, !str.isEmpty else {
+        func readProxy(_ key: String) {
+            guard let str = environment[key], !str.isEmpty else {
                 return
             }
-            guard let url = URLComponents.init(string: str),
-                let host = url.host,
-                let port = url.port,
-                let scheme = url.scheme else {
+            guard let info = ProxyInfo.parse(str) else {
                 return
             }
-            let useType: ProxyType
-            switch (scheme, type) {
-            case ("http", .http), ("https", .http):
-                useType = .socks5
-            case ("http", .https), ("https", .https):
-                useType = .https
-            case ("socks5", _), ("socks", _):
-                useType = .socks5
-            default:
-                return
-            }
-            setProxy(proxyType: useType, host: host, port: port)
+            setProxy(proxyType: info.type, host: info.host, port: info.port)
         }
-        readProxy(environment["http_proxy"], type: .http)
-        readProxy(environment["https_proxy"], type: .https)
-        readProxy(environment["all_proxy"], type: .socks5)
-        readProxy(environment["HTTP_PROXY"], type: .http)
-        readProxy(environment["HTTPS_PROXY"], type: .https)
-        readProxy(environment["ALL_PROXY"], type: .socks5)
+        ProxyInfo.commonProxyKeys.forEach(readProxy(_:))
     }
     
 }
 #endif
+
+public struct ProxyInfo {
+
+    public static let commonProxyKeys = ["HTTP_PROXY", "http_proxy", "HTTPS_PROXY", "https_proxy", "ALL_PROXY", "all_proxy"]
+
+    public let type: ProxyType
+    public let host: String
+    public let port: Int
+    public let auth: (username: String, password: String)?
+
+    public enum ProxyType: String {
+        case http
+        case https
+        case socks5
+    }
+
+    public init(type: ProxyInfo.ProxyType, host: String, port: Int, auth: (username: String, password: String)?) {
+        self.type = type
+        self.host = host
+        self.port = port
+        self.auth = auth
+    }
+
+    public static func parse(_ str: String, defaultType: ProxyType? = .http) -> Self? {
+        guard let url = URLComponents(string: str),
+            let host = url.host,
+            let port = url.port else {
+                return nil
+        }
+        let type: ProxyType
+        if let scheme = url.scheme?.lowercased() {
+            if let v = ProxyType(rawValue: scheme) {
+                type = v
+            } else {
+                return nil
+            }
+        } else if let v = defaultType {
+            type = v
+        } else {
+            return nil
+        }
+
+        var auth: (username: String, password: String)?
+        if let u = url.user, let p = url.password {
+            auth = (u, p)
+        }
+
+        return .init(type: type, host: host, port: port, auth: auth)
+    }
+}

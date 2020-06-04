4fc0f444191e1b03d0d3246b3b3eef7def270b3d
kojirou <kojirouhtc@gmail.com>
2020-06-05 01:09:41 +0800

    update AnyCodingKey to use TwoCase

--- Sources/KwiftUtility/AnyCodingKey.swift
+++ Sources/KwiftUtility/AnyCodingKey.swift
@@ -1,18 +1,38 @@
-public struct AnyCodingKey: CodingKey {
-    
-    public let key: String
-    
-    public init(stringValue: String) {
-        key = stringValue
+public struct AnyCodingKey: CodingKey, Equatable {
+
+  public let value: TwoCase<String, Int>
+
+  public init(stringValue: String) {
+    value = .first(stringValue)
+  }
+
+  public init(intValue: Int) {
+    value = .second(intValue)
+  }
+
+  public var stringValue: String {
+    switch value {
+    case .first(let s): return s
+    case .second(let i): return i.description
     }
-    
-    public init(_ key: String) {
-        self.key = key
-    }
-    
-    public var stringValue: String { key }
-    
-    public var intValue: Int? { nil }
-    
-    public init?(intValue: Int) { nil }
+  }
+
+  public var intValue: Int? { value.secondValue }
+
+}
+
+extension AnyCodingKey: ExpressibleByStringLiteral {
+
+  public init(stringLiteral value: String) {
+    self.value = .first(value)
+  }
+  
+}
+
+extension AnyCodingKey: ExpressibleByIntegerLiteral {
+
+  public init(integerLiteral value: Int) {
+    self.value = .second(value)
+  }
+
 }

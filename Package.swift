caaee032464100c923d6e648c713de8b9b69d108
kojirou <kojirouhtc@gmail.com>
2020-06-04 20:06:44 +0800

    bump swift-tools-version to 5.2

--- Package.swift
+++ Package.swift
@@ -1,30 +1,33 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
-    name: "Kwift",
-    products: [
-        .library(
-            name: "Kwift",
-            targets: ["KwiftUtility", "KwiftExtension"]),
-        .library(
-            name: "KwiftUtility",
-            targets: ["KwiftUtility"]),
-        .library(
-            name: "KwiftExtension",
-            targets: ["KwiftExtension"])
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "KwiftExtension",
-            dependencies: []),
-        .target(
-            name: "KwiftUtility",
-            dependencies: ["KwiftExtension"]),
-        .testTarget(
-            name: "KwiftTests",
-            dependencies: ["KwiftUtility"])
-    ]
+  name: "Kwift",
+  platforms: [
+    .macOS(.v10_15)
+  ],
+  products: [
+    .library(
+      name: "Kwift",
+      targets: ["KwiftUtility", "KwiftExtension"]),
+    .library(
+      name: "KwiftUtility",
+      targets: ["KwiftUtility"]),
+    .library(
+      name: "KwiftExtension",
+      targets: ["KwiftExtension"])
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "KwiftExtension",
+      dependencies: []),
+    .target(
+      name: "KwiftUtility",
+      dependencies: ["KwiftExtension"]),
+    .testTarget(
+      name: "KwiftTests",
+      dependencies: ["KwiftUtility"])
+  ]
 )

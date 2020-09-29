538cdeda9e77c6fc7ed4df113df2bd1aefd51f21
kojirou <kojirouhtc@gmail.com>
2020-09-30 01:53:38 +0800

    more c bool convert

--- Sources/KwiftExtension/Bool+.swift
+++ Sources/KwiftExtension/Bool+.swift
@@ -2,7 +2,7 @@ extension Bool {
 
   /// c bool convertion
   /// - Parameter cValue: 0 is false
-  @inlinable
+  @inlinable @_transparent
   public init<T: FixedWidthInteger>(cValue: T) {
     self = cValue != 0
   }
@@ -10,9 +10,16 @@ extension Bool {
 }
 
 extension FixedWidthInteger {
-  @inlinable
   /// 0 is false
+  @inlinable @_transparent
   public var cBool: Bool {
     self != 0
+  }
+
+  /// false is 0
+  /// - Parameter cBool: swift Bool
+  @inlinable @_transparent
+  public init(cBool: Bool) {
+    self = cBool ? 1 : 0
   }
 }
